10ec900491aa5e9393a9cb4b66291fc6a3b70d46
vano2903 <davidevanoncini2003@gmail.com>
2023-12-03 09:27:19 +0100

    day 3 2023 p2 wip

--- 2023/3/main.go
+++ 2023/3/main.go
@@ -92,7 +92,7 @@ func solve1(lines []string) {
 					}
 				}
 			}
-			if !isInNumber && len(numberChars) > 1 {
+			if !isInNumber && len(numberChars) > 0 {
 				if valid {
 					n := strings.Join(numberChars, "")
 					number, _ := strconv.Atoi(n)
@@ -108,7 +108,7 @@ func solve1(lines []string) {
 				valid = false
 			}
 		}
-		if len(numberChars) > 1 {
+		if len(numberChars) > 0 {
 			if valid {
 				n := strings.Join(numberChars, "")
 				number, _ := strconv.Atoi(n)
@@ -123,7 +123,168 @@ func solve1(lines []string) {
 	fmt.Println(sum)
 }
 
+func solve2(lines []string) {
+	sum := 0
+	for posY, v := range lines {
+		for posX, char := range v {
+			// fmt.Println("current char:", string(char), "is number:", unicode.IsNumber(char), "chars:", strings.Join(numberChars, ""))
+			couldBeGear := char == '*'
+			if couldBeGear {
+				if posY != 0 && posX != 0 {
+					topLeft := rune(lines[posY-1][posX-1])
+					if !unicode.IsNumber(topLeft) {
+						//fmt.Println(strings.Join(numberChars, ""), "is valid cause of top left")
+						valid = true
+					}
+				}
+				if posY != 0 {
+					top := rune(lines[posY-1][posX])
+					if !unicode.IsNumber(top) {
+						//fmt.Println(strings.Join(numberChars, ""), "is valid cause of top")
+						valid = true
+					}
+				}
+
+				if posY != 0 && posX != len(v)-1 {
+					topRight := rune(lines[posY-1][posX+1])
+					if !unicode.IsNumber(topRight) {
+						//fmt.Println(strings.Join(numberChars, ""), "is valid cause of top right")
+						valid = true
+					}
+				}
+
+				if posX != 0 {
+					left := rune(v[posX-1])
+					if !unicode.IsNumber(left) {
+						//fmt.Println(strings.Join(numberChars, ""), "is valid cause of left")
+						valid = true
+					}
+				}
+				if posX != len(v)-1 {
+					right := rune(v[posX+1])
+					if !unicode.IsNumber(right) {
+						//fmt.Println(strings.Join(numberChars, ""), "is valid cause of right")
+						valid = true
+					}
+				}
+
+				if posY != len(lines)-1 && posX != 0 {
+					bottomLeft := rune(lines[posY+1][posX-1])
+					if !unicode.IsNumber(bottomLeft) {
+						//fmt.Println(strings.Join(numberChars, ""), "is valid cause of bottom left")
+						valid = true
+					}
+				}
+				if posY != len(lines)-1 {
+					bottom := rune(lines[posY+1][posX])
+					if !unicode.IsNumber(bottom) {
+						//fmt.Println(strings.Join(numberChars, ""), "is valid cause of bottom")
+						valid = true
+					}
+				}
+
+				if posY != len(lines)-1 && posX != len(v)-1 {
+					bottomRight := rune(lines[posY+1][posX+1])
+					if !unicode.IsNumber(bottomRight) {
+						//fmt.Println(strings.Join(numberChars, ""), "is valid cause of bottom right")
+						valid = true
+					}
+				}
+			}
+				if posY != 0 && posX != 0 {
+					topLeft := rune(lines[posY-1][posX-1])
+					if !unicode.IsNumber(topLeft) {
+						//fmt.Println(strings.Join(numberChars, ""), "is valid cause of top left")
+						valid = true
+					}
+				}
+				if posY != 0 {
+					top := rune(lines[posY-1][posX])
+					if !unicode.IsNumber(top) {
+						//fmt.Println(strings.Join(numberChars, ""), "is valid cause of top")
+						valid = true
+					}
+				}
+
+				if posY != 0 && posX != len(v)-1 {
+					topRight := rune(lines[posY-1][posX+1])
+					if !unicode.IsNumber(topRight) {
+						//fmt.Println(strings.Join(numberChars, ""), "is valid cause of top right")
+						valid = true
+					}
+				}
+
+				if posX != 0 {
+					left := rune(v[posX-1])
+					if !unicode.IsNumber(left) && left != '.' {
+						//fmt.Println(strings.Join(numberChars, ""), "is valid cause of left")
+						valid = true
+					}
+				}
+				if posX != len(v)-1 {
+					right := rune(v[posX+1])
+					if !unicode.IsNumber(right) && right != '.' {
+						//fmt.Println(strings.Join(numberChars, ""), "is valid cause of right")
+						valid = true
+					}
+				}
+
+				if posY != len(lines)-1 && posX != 0 {
+					bottomLeft := rune(lines[posY+1][posX-1])
+					if !unicode.IsNumber(bottomLeft) && bottomLeft != '.' {
+						//fmt.Println(strings.Join(numberChars, ""), "is valid cause of bottom left")
+						valid = true
+					}
+				}
+				if posY != len(lines)-1 {
+					bottom := rune(lines[posY+1][posX])
+					if !unicode.IsNumber(bottom) && bottom != '.' {
+						//fmt.Println(strings.Join(numberChars, ""), "is valid cause of bottom")
+						valid = true
+					}
+				}
+
+				if posY != len(lines)-1 && posX != len(v)-1 {
+					bottomRight := rune(lines[posY+1][posX+1])
+					if !unicode.IsNumber(bottomRight) && bottomRight != '.' {
+						//fmt.Println(strings.Join(numberChars, ""), "is valid cause of bottom right")
+						valid = true
+					}
+				}
+			}
+			if !isInNumber && len(numberChars) > 0 {
+				if valid {
+					n := strings.Join(numberChars, "")
+					number, _ := strconv.Atoi(n)
+					sum += number
+					fmt.Println(number, "is valid")
+				} else {
+					n := strings.Join(numberChars, "")
+					fmt.Println(n, "is not valid")
+				}
+			}
+			if !isInNumber {
+				numberChars = make([]string, 0)
+				valid = false
+			}
+		}
+		if len(numberChars) > 0 {
+			if valid {
+				n := strings.Join(numberChars, "")
+				number, _ := strconv.Atoi(n)
+				sum += number
+				fmt.Println(number, "is valid")
+			} else {
+				n := strings.Join(numberChars, "")
+				fmt.Println(n, "is not valid")
+			}
+		}
+	}
+}
+
 func main() {
-	l := readFile("a.txt")
+	l := readFile("input.txt")
 	solve1(l)
+	//539713
+	//539651
 }
